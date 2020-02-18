2c7872bd57e75c945f97c6641cc8dfce5332dda6
Aman Lalpuria <Lalpuria_Am@India.XoriantCorp.com>
2020-02-18 10:46:04 +0530

    Added the problem statement

--- find-in-jar-unzip.sh
+++ find-in-jar-unzip.sh
@@ -1,3 +1,8 @@
+# This script help to search the file in multiple jars using jar command
+# "D:/Practice/Jars/*.jars" jar location 
+# unzip -l help to list out all the files of the jar
+# grep to check the file is present 
+
 for jar_file in D:/Practice/Jars/*.jar; do
 
 	jar_contain=$(unzip -l $jar_file |grep $1)
